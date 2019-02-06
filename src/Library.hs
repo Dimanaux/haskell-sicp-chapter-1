411ef5566fc319bb65c182dfe491a22dfd6d265a
cosmoskey <barskoidim@gmail.com>
2019-02-06 10:13:06 +0300

    add approximately func to Library.hs

--- src/Library.hs
+++ src/Library.hs
@@ -1,8 +1,18 @@
 module Library
-    ( cond
+    (
+      cond
+    , approximately
     ) where
 
 -- LISP style function cond
 cond :: [(Bool, a)] -> a
 cond [] = error "Non-exhaustive conditions. At least 1 condition must be true."
 cond (x:xs) = if fst x then snd x else cond xs
+
+-- equality epsilon comparison
+epsilonComparison :: Float -> Float -> Float -> Bool
+epsilonComparison epsilon a b = abs (a - b) < epsilon
+
+-- uses comparison with 0.0001 precision
+approximately :: Float -> Float -> Bool
+approximately = epsilonComparison 1e-4
