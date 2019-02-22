d262b176d2ac97e643a982675a8f1317cf007880
Dmitry Barskov <barskoidim@gmail.com>
2019-02-22 09:24:34 +0300

    solve 1.17

---
+++ src/Exercise17.hs
@@ -0,0 +1,43 @@
+module Exercise17
+    (
+      fastMul
+    ) where
+
+{- Exercise 1.17.
+-- The exponentiation algorithms in this section are based
+-- on performing exponentiation by means of repeated
+-- multiplication. In a similar way, one can perform
+-- integer multiplication by means of repeated addition.
+-- The following multiplication procedure (in which it is
+-- assumed that our language can only add, not multiply) is
+-- analogous to the expt procedure:
+
+(define (* a b)
+  (if (= b 0)
+      0
+      (+ a (* a (- b 1)))))
+
+-- This algorithm takes a number of steps that is linear in
+-- b. Now suppose we include, together with addition,
+-- operations double, which doubles an integer, and halve,
+-- which divides an (even) integer by 2. Using these, design
+-- a multiplication procedure analogous to fast-expt that
+-- uses a logarithmic number of steps.
+-}
+
+import Library (halve, double)
+
+-- in Haskell:
+mul :: Int -> Int -> Int
+mul a b =
+    if b == 0
+        then 0
+        else a + mul a (b - 1)
+
+-- Soltuion:
+fastMul :: Int -> Int -> Int
+fastMul 0 _  = 0
+fastMul _ 0 = 0
+fastMul x y
+    | even y = double (fastMul x (halve y))
+    | otherwise = x + (fastMul x (y - 1))
