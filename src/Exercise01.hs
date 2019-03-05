eb8fe7f5870747e5f2cda80246c8e067d711b040
Dmitry Barskov <barskoidim@gmail.com>
2019-03-05 13:04:44 +0300

    beautify exercise 1.1 solution

--- src/Exercise01.hs
+++ src/Exercise01.hs
@@ -21,29 +21,36 @@ import Library
 
 -- !NOTE run via $ stack runghc -- src/Exercise01.hs
 
-main :: IO()
+main :: IO ()
 main = do
     print 10 -- 10
     -- print (5 + 3 + 4)
     print ((+) 5 ((+) 3 4)) -- 12
     -- print (9 - 1)
     print ((-) 9 1) -- 8
-    -- print ( (2 + 4) * (4 - 6) ) -- and so on...
+    -- print ( (2 + 4) * (4 - 6) )
     print ((+) ((*) 2 4) ((-) 4 6)) -- 6
     let a = 3
+    -- let b = a + 1
     let b = (+) a 1
-    -- print (a + b + (a * b))
+    -- print (a + b + a * b)
     print ((+) a ((+) b ((*) a b))) -- 19
     -- print (a == b)
     print ((==) a b) -- False
-    -- .......((b > a) && (b < a * b)) then b else a
+    -- if b > a && b < a * b then b else a
     print (if ((&&) ((>) b a) ((<) b ((*) a b))) then b else a) -- 4
     -- there is no such statement COND in Haskell, but we can create a
     -- function cond, that takes a list of pairs (condition, expression)
     -- doing the same
+    -- cond [(a == 4, 6)
+    --      ,(b == 4, 6 + 7 + a)
+    --      ,(otherwise, 0)]
     print (cond [((==) a 4 , 6                 )
                 ,((==) b 4 , ((+) 6 ((+) 7 a)) )
                 ,(otherwise, 0                 ) ]) -- 16
+    -- (cond [(a > b, a)
+    --       ,(a < b, b)
+    --       ,(otherwise, -1)]) * (a + 1)
     print ((*) (cond [((>) a b  , a )
                      ,((<) a b  , b )
                      ,(otherwise, -1) ])
